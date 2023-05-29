4468993cc01eddf1ecf2f91f254f47afb9b5528e
viviennoel <vivien.noel@live.fr>
2023-05-29 18:13:20 +0200

    fix: update endpoint prismic

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,21 +23,11 @@ export default Index;
 
 export async function getStaticProps({ previewData }) {
   const client = createClient({ previewData });
-
-  const articles = await client.getAllByType("article", {
-    orderings: [
-      { field: "my.article.publishDate", direction: "desc" },
-      { field: "document.first_publication_date", direction: "desc" },
-    ],
-  });
-  const navigation = await client.getSingle("navigation");
-  const settings = await client.getSingle("settings");
-
+  const header = await client.getSingle("header");
+  
   return {
     props: {
-      articles,
-      navigation,
-      settings,
+      header
     },
   };
 }

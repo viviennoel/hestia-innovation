c2af621678a0f0d9b11cc91c01db2aabc630e59a
viviennoel <vivien.noel@live.fr>
2023-06-21 21:23:39 +0200

    fix: remove context from contentHighlight

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
 import { BannerHome } from '../components/organism/BannerHome';
 import { Highlights } from '../components/organism/Highlights';
-import { contentHighlights } from '../components/organism/Highlights/contentHighlights';
 import LanguageContext from '../context/languageContext';
 import { translations } from '../translations/translations';
 import { BannerImage } from './../components/organism/BannerImage';
@@ -16,10 +15,34 @@ const Index = () => {
       setLanguage(localLanguage);
     }, [language]);
 
+    const contentHighlight = [
+        {
+            source: 'https://res.cloudinary.com/djlwtz7qw/image/upload/v1615571569/samples/animals/reindeer.jpg',
+            title: translations[language].titleAI,
+            text:translations[language].subtitleAI,
+            link:'',
+            delay:'0'
+        },
+        {
+            source:'https://res.cloudinary.com/djlwtz7qw/image/upload/v1615571569/samples/animals/reindeer.jpg',
+            title: translations[language].titleDesign,
+            text:translations[language].subtitleDesign,
+            link:'/',
+            delay:'300'
+        },
+        {
+            source:'https://res.cloudinary.com/djlwtz7qw/image/upload/v1615571569/samples/animals/reindeer.jpg',
+            title: translations[language].titleGoodPractices,
+            text:translations[language].subtitleGoodPractices,
+            link:'/',
+            delay:'600'
+        }
+    ]
+
     return(
         <div>
             <BannerHome />
-            <Highlights content={contentHighlights()}/>
+            <Highlights content={contentHighlight}/>
             <BannerTextImage
                 imageSrc='https://res.cloudinary.com/djlwtz7qw/image/upload/v1684920923/cld-sample-2.jpg'
                 title={translations[language].HomePresentation.title} 

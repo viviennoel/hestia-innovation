9236151640ca54647879ba15b043ef1d8f4ed2b7
viviennoel <52512132+viviennoel@users.noreply.github.com>
2023-07-27 17:13:24 +0200

    Create PrivacyPolicy.ts

---
+++ translations/legal/PrivacyPolicy.ts
@@ -0,0 +1,202 @@
+export const PrivacyPolicy = {
+  "en-GB": {
+  "title": "Legal Mentions",
+  "src": "https://www.hestia-innovation.com/legal",
+  "paragraphs": [
+    {
+      "title": "Disclaimer",
+      "body": [
+        "Subtitle: Website Information",
+        "Paragraph: This website is owned and operated by Hestia Innovation.",
+
+        "Subtitle: Information Accuracy",
+        "Paragraph: The information provided on this website is for general informational purposes only. While we strive to keep the information up to date and accurate, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.",
+
+        "Subtitle: Limitation of Liability",
+        "Paragraph: In no event will we be liable for any loss or damage including, without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.",
+
+        "Subtitle: External Links",
+        "Paragraph: Through this website, you can link to other websites that are not under the control of Hestia Innovation. We have no control over the nature, content, and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.",
+
+        "Subtitle: Website Availability",
+        "Paragraph: Every effort is made to keep the website up and running smoothly. However, Hestia Innovation takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control."
+      ]
+    },
+    {
+      "title": "Intellectual Property",
+      "body": [
+        "Subtitle: Content Ownership",
+        "Paragraph: The content and materials provided on this website, including but not limited to text, graphics, logos, images, audio clips, digital downloads, and data compilations, are the property of Hestia Innovation or its content suppliers and protected by applicable copyright laws.",
+
+        "Subtitle: Unauthorized Use",
+        "Paragraph: Any unauthorized use, including but not limited to reproduction, distribution, display, or transmission of the content of this website is strictly prohibited unless authorized in writing by Hestia Innovation.",
+
+        "Subtitle: Trademarks",
+        "Paragraph: All trademarks reproduced on this website, which are not the property of, or licensed to Hestia Innovation, are acknowledged on the website.",
+
+        "Subtitle: Consequences of Unauthorized Use",
+        "Paragraph: Unauthorized use of this website may give rise to a claim for damages and/or be a criminal offense."
+      ]
+    },
+    {
+      "title": "Privacy Policy",
+      "body": [
+        "Subtitle: Your Privacy is Important to Us",
+        "Paragraph: This Privacy Policy explains how Hestia Innovation uses Google Analytics to collect and process data when you visit our website. We are committed to safeguarding the privacy of our website visitors and ensuring that your personal information is protected.",
+        "Subtitle: Consent",
+        "Paragraph: By using our website, you consent to the use of Google Analytics as described in this Privacy Policy. If you do not agree with this Policy, please do not use our website.",
+        "Subtitle: Definitions",
+        "Paragraph: In this Privacy Policy, the terms 'we', 'us', and 'Hestia Innovation' refer to our web consulting agency. 'You' refers to the user or visitor of our website."
+      ]
+    },
+    {
+      "title": "Information Collected by Google Analytics",
+      "body": [
+        "Subtitle: What is Google Analytics?",
+        "Paragraph: Google Analytics is a web analytics service provided by Google that tracks and reports website traffic. It helps us understand how visitors interact with our website, allowing us to improve user experience and enhance our services.",
+        "Subtitle: Data Collected",
+        "Paragraph: Google Analytics collects various types of data, including but not limited to your IP address, device information, browser type, operating system, location, and website usage information. This data is aggregated and anonymized, meaning it does not personally identify you.",
+        "Subtitle: Cookies and Similar Technologies",
+        "Paragraph: Google Analytics uses cookies and similar technologies to track and analyze website usage. Cookies are small text files placed on your device to collect standard internet log information and visitor behavior information. You can control the use of cookies through your browser settings.",
+        "Subtitle: Opt-out",
+        "Paragraph: If you do not wish to be tracked by Google Analytics, you can opt-out by installing the Google Analytics opt-out browser add-on. This add-on prevents Google Analytics JavaScript (ga.js, analytics.js, and dc.js) from sharing information with Google Analytics about your activity.",
+        "Subtitle: For more information",
+        "Paragraph: To learn more about Google Analytics and how it handles data privacy, please review Google's Privacy Policy: https://policies.google.com/privacy"
+      ]
+    },
+    {
+      "title": "How We Use the Collected Information",
+      "body": [
+        "Subtitle: Purpose of Data Collection",
+        "Paragraph: We use the information collected through Google Analytics to analyze website traffic, track user behavior, and improve our website's content and services. This data helps us better understand our audience and tailor our offerings to your preferences.",
+        "Subtitle: Data Retention",
+        "Paragraph: Google Analytics retains the collected data for a period determined by Google. We do not control the data retention period, and the information is subject to Google's Privacy Policy.",
+        "Subtitle: Third-party Disclosure",
+        "Paragraph: We do not sell, trade, or transfer your personally identifiable information to third parties. However, we may share aggregated and anonymized data with our partners, service providers, or other entities for analytical purposes.",
+        "Subtitle: Legal Requirements",
+        "Paragraph: We may disclose your information to comply with legal obligations, enforce our website policies, protect our rights, or respond to government requests or legal processes.",
+        "Subtitle: Data Security",
+        "Paragraph: We take reasonable precautions to protect the data collected through Google Analytics. However, please note that no method of data transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.",
+        "Subtitle: Children's Privacy",
+        "Paragraph: Our website is not intended for children under the age of 13, and we do not knowingly collect personal information from children. If you are a parent or guardian and believe that your child has provided us with personal information, please contact us, and we will take appropriate measures to remove the information from our records."
+      ]
+    },
+    {
+      "title": "Changes to This Privacy Policy",
+      "body": [
+        "Subtitle: Policy Updates",
+        "Paragraph: We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. Any updates will be posted on this page, and the 'Last Updated' date at the top of the Policy will be revised.",
+        "Subtitle: Your Continued Use",
+        "Paragraph: By continuing to use our website after any changes to this Privacy Policy, you accept the updated version and agree to the terms outlined therein."
+      ]
+    },
+    {
+      "title": "Contact Us",
+      "body": [
+        "Subtitle: Questions and Concerns",
+        "Paragraph: If you have any questions, concerns, or requests related to this Privacy Policy, please contact us at [Your Web Consulting Agency's Contact Information]. We will do our best to address your inquiries in a timely and satisfactory manner."
+      ]
+    } 
+  ]
+},
+"fr-FR": {
+"title": "Mentions Légales",
+"src": "https://www.hestia-innovation.com/legal",
+"paragraphs": [
+{
+"title": "Clause de non-responsabilité",
+"body": [
+"Subtitle: Informations sur le site web",
+"Paragraphe: Ce site web est détenu et exploité par Hestia Innovation.",
+    "Subtitle: Exactitude des informations",
+    "Paragraphe: Les informations fournies sur ce site web le sont à titre informatif général uniquement. Bien que nous nous efforcions de maintenir les informations à jour et exactes, nous ne faisons aucune déclaration ni garantie d'aucune sorte, explicite ou implicite, concernant l'exhaustivité, l'exactitude, la fiabilité, l'adéquation ou la disponibilité concernant le site web ou les informations, produits, services ou graphiques associés présents sur le site web, dans un but quelconque. Toute confiance que vous accordez à de telles informations est donc strictement à vos propres risques.",
+
+    "Subtitle: Limitation de responsabilité",
+    "Paragraphe: En aucun cas, nous ne serons responsables de toute perte ou dommage, y compris, sans limitation, les pertes ou dommages indirects ou consécutifs, ou toute perte ou dommage découlant de la perte de données ou de bénéfices résultant de l'utilisation de ce site web.",
+
+    "Subtitle: Liens externes",
+    "Paragraphe: Par le biais de ce site web, vous pouvez accéder à d'autres sites web qui ne sont pas sous le contrôle de Hestia Innovation. Nous n'avons aucun contrôle sur la nature, le contenu et la disponibilité de ces sites. L'inclusion de tout lien n'implique pas nécessairement une recommandation ou une approbation des points de vue exprimés en leur sein.",
+
+    "Subtitle: Disponibilité du site web",
+    "Paragraphe: Tous les efforts sont faits pour maintenir le bon fonctionnement du site web. Cependant, Hestia Innovation n'assume aucune responsabilité en cas d'indisponibilité temporaire du site web due à des problèmes techniques indépendants de notre volonté."
+  ]
+},
+{
+  "title": "Propriété Intellectuelle",
+  "body": [
+    "Subtitle: Propriété du contenu",
+    "Paragraphe: Le contenu et les matériaux fournis sur ce site web, y compris, sans s'y limiter, le texte, les graphiques, les logos, les images, les extraits audio, les téléchargements numériques et les compilations de données, sont la propriété de Hestia Innovation ou de ses fournisseurs de contenu et sont protégés par les lois sur le droit d'auteur applicables.",
+
+    "Subtitle: Utilisation non autorisée",
+    "Paragraphe: Toute utilisation non autorisée, y compris, mais sans s'y limiter, la reproduction, la distribution, l'affichage ou la transmission du contenu de ce site web est strictement interdite, sauf autorisation écrite de Hestia Innovation.",
+
+    "Subtitle: Marques de commerce",
+    "Paragraphe: Toutes les marques de commerce reproduites sur ce site web, qui ne sont pas la propriété de Hestia Innovation ou qui ne sont pas autorisées par celle-ci, sont reconnues sur le site web.",
+
+    "Subtitle: Conséquences de l'utilisation non autorisée",
+    "Paragraphe: L'utilisation non autorisée de ce site web peut entraîner des réclamations en dommages et intérêts et/ou constituer une infraction pénale."
+  ]
+},
+{
+  "title": "Politique de Confidentialité",
+  "body": [
+    "Subtitle: Votre vie privée nous importe",
+    "Paragraphe: Cette Politique de Confidentialité explique comment Hestia Innovation utilise Google Analytics pour collecter et traiter des données lorsque vous visitez notre site web. Nous nous engageons à protéger la confidentialité de nos visiteurs du site web et à assurer la protection de vos informations personnelles.",
+    "Subtitle: Consentement",
+    "Paragraphe: En utilisant notre site web, vous consentez à l'utilisation de Google Analytics conformément à cette Politique de Confidentialité. Si vous n'êtes pas d'accord avec cette Politique, veuillez ne pas utiliser notre site web.",
+    "Subtitle: Définitions",
+    "Paragraphe: Dans cette Politique de Confidentialité, les termes 'nous', 'notre' et 'Hestia Innovation' font référence à notre agence de conseil en ligne. 'Vous' désigne l'utilisateur ou le visiteur de notre site web."
+  ]
+},
+{
+  "title": "Informations Collectées par Google Analytics",
+  "body": [
+    "Subtitle: Qu'est-ce que Google Analytics ?",
+    "Paragraphe: Google Analytics est un service d'analyse web fourni par Google qui suit et rapporte le trafic sur les sites web. Il nous aide à comprendre comment les visiteurs interagissent avec notre site web, ce qui nous permet d'améliorer l'expérience utilisateur et d'enrichir nos services.",
+    "Subtitle: Données Collectées",
+    "Paragraphe: Google Analytics collecte différents types de données, y compris, mais sans s'y limiter, votre adresse IP, des informations sur votre appareil, le type de navigateur, le système d'exploitation, la localisation et des informations sur l'utilisation du site web. Ces données sont agrégées et anonymisées, ce qui signifie qu'elles ne permettent pas de vous identifier personnellement.",
+    "Subtitle: Cookies et Technologies Similaires",
+    "Paragraphe: Google Analytics utilise des cookies et des technologies similaires pour suivre et analyser l'utilisation du site web. Les cookies sont de petits fichiers texte placés sur votre appareil pour collecter des informations standard sur les journaux d'activité Internet et le comportement des visiteurs. Vous pouvez contrôler l'utilisation des cookies via les paramètres de votre navigateur.",
+    "Subtitle: Désinscription",
+    "Paragraphe: Si vous ne souhaitez pas être suivi par Google Analytics, vous pouvez vous désinscrire en installant le module complémentaire de navigateur de désinscription Google Analytics. Ce module complémentaire empêche le partage d'informations concernant votre activité avec Google Analytics via JavaScript (ga.js, analytics.js et dc.js).",
+    "Subtitle: Pour plus d'informations",
+    "Paragraphe: Pour en savoir plus sur Google Analytics et la manière dont il gère la confidentialité des données, veuillez consulter la Politique de Confidentialité de Google : https://policies.google.com/privacy"
+  ]
+},
+{
+  "title": "Utilisation des Informations Collectées",
+  "body": [
+    "Subtitle: Objectif de la Collecte de Données",
+    "Paragraphe: Nous utilisons les informations collectées via Google Analytics pour analyser le trafic du site web, suivre le comportement des utilisateurs et améliorer le contenu et les services de notre site web. Ces données nous aident à mieux comprendre notre public et à adapter nos offres à vos préférences.",
+    "Subtitle: Conservation des Données",
+    "Paragraphe: Google Analytics conserve les données collectées pendant une période déterminée par Google. Nous n'avons pas le contrôle sur la période de conservation des données, et les informations sont soumises à la Politique de Confidentialité de Google.",
+    "Subtitle: Divulgation à des Tiers",
+    "Paragraphe: Nous ne vendons, n'échangeons ni ne transférons vos informations personnelles identifiables à des tiers. Cependant, nous pouvons partager des données agrégées et anonymisées avec nos partenaires, prestataires de services ou autres entités à des fins d'analyse.",
+    "Subtitle: Obligations Légales",
+    "Paragraphe: Nous pouvons divulguer vos informations pour nous conformer à des obligations légales, faire respecter nos politiques du site web, protéger nos droits ou répondre aux demandes gouvernementales ou aux procédures juridiques.",
+    "Subtitle: Sécurité des Données",
+    "Paragraphe: Nous prenons des précautions raisonnables pour protéger les données collectées via Google Analytics. Cependant, veuillez noter qu'aucune méthode de transmission de données sur Internet ou de stockage électronique n'est 100% sécurisée, et nous ne pouvons garantir une sécurité absolue.",
+    "Subtitle: Confidentialité des Enfants",
+    "Paragraphe: Notre site web n'est pas destiné aux enfants de moins de 13 ans, et nous ne collectons pas sciemment d'informations personnelles auprès des enfants. Si vous êtes parent ou tuteur et que vous pensez que votre enfant nous a fourni des informations personnelles, veuillez nous contacter, et nous prendrons les mesures appropriées pour supprimer les informations de nos dossiers."
+  ]
+},
+{
+  "title": "Modifications de cette Politique de Confidentialité",
+  "body": [
+    "Subtitle: Mises à jour de la Politique",
+    "Paragraphe: Nous pouvons mettre à jour cette Politique de Confidentialité de temps à autre pour refléter les changements dans nos pratiques ou pour d'autres raisons opérationnelles, légales ou réglementaires. Toutes les mises à jour seront publiées sur cette page, et la date de 'Dernière mise à jour' en haut de la Politique sera révisée.",
+    "Subtitle: Votre Utilisation Continue",
+    "Paragraphe: En continuant à utiliser notre site web après toute modification de cette Politique de Confidentialité, vous acceptez la version mise à jour et vous engagez à respecter les termes qui y sont décrits."
+  ]
+},
+{
+  "title": "Contactez-nous",
+  "body": [
+    "Subtitle: Questions et Préoccupations",
+    "Paragraphe: Si vous avez des questions, des préoccupations ou des demandes concernant cette Politique de Confidentialité, veuillez nous contacter à [Les coordonnées de votre agence de conseil en ligne]. Nous ferons de notre mieux pour répondre à vos demandes de manière rapide et satisfaisante."
+  ]
+}
+]
+},
+"en-SET": 
+}
